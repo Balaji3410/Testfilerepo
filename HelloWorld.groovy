30ce45da05927a645a422a30d536ca12e59152b2
Balaji3410 <48978007+Balaji3410@users.noreply.github.com>
2020-05-10 15:52:58 +0530

    Update HelloWorld.groovy

--- HelloWorld.groovy
+++ HelloWorld.groovy
@@ -1,11 +1,11 @@
-node('DOTNET'){
-  stage('Build'){
-    echo 'Building.....'
-  }
-  stage('Test'){
-    echo 'Testing.....'
-  }
-  stage('Deploy'){
-    echo 'Deploying.....'
-  } 
+public class Hello{
+
+    public static void main(String[] args){
+
+           for(int i=1;i<=10;i++){
+            System.out.println("Hello Balaji's world.....");
+
+        }
+     }
+
 }
